0e8ed2547672cbee5c8a08a2cb67887b463792ff
Biruk Ephrem <birukeph@gmail.com>
2023-01-06 00:08:53 +0300

    python variable annotations   - did all tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""type-annoted function"""
+
+
+def add(a: float, b: float) -> float:
+    """type noted add function"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""type annoted"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate string"""
+    return str1 + str2
